ba7dd81f25dfdf4de926d8f02a71718034c99912
Sergio Monteiro <smonteiro@odoogap.com>
2020-12-04 18:04:28 +0000

    [IMP] centralized the headers dictonary creation

--- build/lib.linux-x86_64-2.7/odoogap_mtd/connection.py
+++ build/lib.linux-x86_64-2.7/odoogap_mtd/connection.py
@@ -1,11 +1,13 @@
 import odoorpc
 
 
-#def open_connection_odoogap(user, password, server, db, port):
-#    # odoo_instance = odoorpc.ODOO(server, protocol='jsonrpc+ssl', port=int(port))
-#    odoo_instance = odoorpc.ODOO(server, protocol='jsonrpc', port=int(port))
-#   odoo_instance.login(db, user, password)     
-#    return odoo_instance
+def open_connection_odoogap(user, password, server, db, port):
+    # odoo_instance = odoorpc.ODOO(server, protocol='jsonrpc+ssl', port=int(port))
+    odoo_instance = odoorpc.ODOO(server, protocol='jsonrpc', port=int(port))
+    odoo_instance.login(db, user, password)     
+    return odoo_instance
+
+
 
 # def get_token(user, password, server, db, port):
 #    con = open_connection_odoogap(user, password, server, db, port)
